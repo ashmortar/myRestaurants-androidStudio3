643afc56bfd66b1f4f541a0f67cba1c29110da6c
Kyle Tenzeldam and Zach Evans <zach@>
2018-03-12 11:13:22 -0700

    Add testing.

--- app/src/main/java/com/example/guest/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants/RestaurantsActivity.java
@@ -3,15 +3,21 @@ package com.example.guest.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+//    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+//        mListView = (ListView) findViewById(R.id.listview);
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+//        mListView.setAdapter(adapter);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
